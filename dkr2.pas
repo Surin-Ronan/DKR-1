f2748aa03150dc7a1acc34d666936477c8d29016
Surin-Ronan <117994884+Surin-Ronan@users.noreply.github.com>
2022-11-11 16:56:05 +0300

    Add files via upload

---
+++ dkr2.pas
@@ -0,0 +1,18 @@
+﻿program dkr2;
+var a, b, x, y, h:real;
+begin
+  readln(a);
+  readln(b);
+  readln(h);
+  x:=a;
+  while x < b do
+  begin
+  if x < -7 then
+  y:= ln(x)
+  else if x<-2 then
+  y:=x**(1 / 3)*cos(2*x) else
+  y:=x**(1 / 3) / x**(1 / 3) +ln(x);
+  writeln(y) ;
+  x:=x+h;
+  end;
+end.
